3b79d99caa9691bc8a3aa0cde70541d2fb2f3218
zinirun <heojj97@naver.com>
2020-05-04 23:35:49 +0900

    Update lab2_bst.c

--- lab2_bst.c
+++ lab2_bst.c
@@ -318,18 +318,18 @@ int lab2_node_remove_fg(lab2_tree *tree, int key) {
     }
     pthread_mutex_unlock(&tree->mutex);
 
-    //pthread_mutex_lock(&tree->mutex);
+    pthread_mutex_lock(&tree->mutex);
     if ((p->left == NULL) && (p->right == NULL)){// does not exist child node
         if (p == tree->root){
             tree->root = NULL;
-            //pthread_mutex_unlock(&tree->mutex);
+            pthread_mutex_unlock(&tree->mutex);
             return LAB2_SUCCESS;
         }
         if (p == q->left)
             q->left = NULL;
         else
             q->right = NULL;
-        //pthread_mutex_unlock(&tree->mutex);
+        pthread_mutex_unlock(&tree->mutex);
     }
     else if ((p->left != NULL) && (p->right == NULL)){// only exist left child node
         if (p == tree->root)
