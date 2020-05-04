3d4e133ee746117727a7386e48fb16de4c4b550f
zinirun <heojj97@naver.com>
2020-05-04 22:00:59 +0900

    Update lab2_sync_types.h

--- include/lab2_sync_types.h
+++ include/lab2_sync_types.h
@@ -63,7 +63,7 @@ typedef struct thread_arg{
  * TODO
  *  You need to implement these functions. 
  */
-void inorder(lab2_tree *tree);
+void inorder(lab2_node *node);
 int lab2_node_print_inorder(lab2_tree *tree);
 lab2_tree *lab2_tree_create();
 lab2_node *lab2_node_create(int key);
